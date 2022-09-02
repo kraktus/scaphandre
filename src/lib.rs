b5ad97c89c71e766802295a95d569812f98461a7
kraktus <kraktus@users.noreply.github.com>
2022-09-03 00:36:24 +0200

    d

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@ fn get_sensor(matches: &ArgMatches) -> Box<dyn Sensor> {
 
 macro_rules! declare_exporters {
     ($header:tt, $sensor_boxed:tt, $exporter_match_flag:tt, $matches:tt, $($name:tt, $exporter:ty,)+) => {$(
-        if let Some(exporter_parameters) = matches.subcommand_matches($name) {
+        if let Some(exporter_parameters) = $matches.subcommand_matches($name) {
             $exporter_match_flag = true;
             if $header {
                 scaphandre_header($name);
